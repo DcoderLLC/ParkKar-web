0d74e983b5d96aa817c3b9ed61919148f9fadc28
DcoderLLC <dcoderllc@gmail.com>
2024-09-25 16:20:37 +0530

    pages routed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,32 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import Tnc from "./pages/Tnc";
+import Notfound from "./pages/Notfound";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/footer";
 
 function App() {
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+    <Box>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" excat element={<Home />} />
+          <Route path="/book" excat element={<Booking />} />
+          <Route path="/blogs" excat element={<Blogs />} />
+          <Route path="/contact" excat element={<Contact />} />
+          <Route path="/tnc" excat element={<Tnc />} />
+          <Route path="*" excat element={<Notfound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Box>
+  );
 }
 
-export default App
+export default App;
